3853e1913af34b51e231f58086c500208d847a9f
Harnam kaur <kharnam11@gmail.com>
2019-11-20 18:52:22 -0500

    Initial Commit

---
+++ MAPD714-Lesson-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714-Lesson-IOS
+//
+//  Created by Harnam kaur on 20/11/19.
+//  Copyright © 2019 Harnam kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
